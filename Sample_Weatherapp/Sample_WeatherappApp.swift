4bc087a2c14a5b21380244e7676210f2193f2bb4
mdigix <grouses_joiner_0i@icloud.com>
2025-04-20 08:43:55 +0900

    Initial Commit

---
+++ Sample_Weatherapp/Sample_WeatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_WeatherappApp.swift
+//  Sample_Weatherapp
+//
+//  Created by mdigix on 2025/04/20.
+//
+
+import SwiftUI
+
+@main
+struct Sample_WeatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sample_WeatherappTests/Sample_WeatherappTests.swift
@@ -0,0 +1,16 @@
+//
+//  Sample_WeatherappTests.swift
+//  Sample_WeatherappTests
+//
+//  Created by mdigix on 2025/04/20.
+//
+
+import Testing
+
+struct Sample_WeatherappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
